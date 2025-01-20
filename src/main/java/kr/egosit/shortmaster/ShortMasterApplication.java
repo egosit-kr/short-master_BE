0cb1e1988f53a161260fdc2b865f1c57731590c1
CSeungJoo <seungjoo13579@gmail.com>
2025-01-20 16:53:48 +0900

    init

---
+++ src/main/java/kr/egosit/shortmaster/ShortMasterApplication.java
@@ -0,0 +1,13 @@
+package kr.egosit.shortmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/egosit/shortmaster/ShortMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.egosit.shortmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
